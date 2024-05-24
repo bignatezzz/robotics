ee1396b170141b72aae46c49e16fbf37e2049a2e
bignatezzz <68629636+bignatezzz@users.noreply.github.com>
2024-05-23 20:04:25 -0700

    Create serialmonitor_potentiometer_controll_LEDpower.ino

---
+++ serialmonitor_potentiometer_controll_LEDpower.ino
@@ -0,0 +1,23 @@
+//float a = 1023;
+//float b = 4;
+//float c = a / b; * can print all this on the serial monitor
+float data = 0;
+float value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600); * only used when need serial monitor
+ // Serial.println(c); *prints variable "c" in serial monitor
+  pinMode(5, OUTPUT);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  data = analogRead(A0);
+  value = data / 4.0;
+  analogWrite(5, value);
+}
+
+
+
